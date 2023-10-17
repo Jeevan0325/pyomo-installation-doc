ea89fe18c2db6ae147c0ca1863f9e2c6685ed35c
Jeevan0325 <107616121+Jeevan0325@users.noreply.github.com>
2023-10-17 15:18:54 +0530

    secound change

--- MTO_Scheduling_v0_change.py
+++ MTO_Scheduling_v0_change.py
@@ -1,3 +1,4 @@
+
 from pyomo.environ import *
 
 # Sample data
@@ -17,19 +18,12 @@ priority_weights = [
 
 # Resource data
 resource_availabilities = [
-    [1] * T,  # Availability for Resource 1
-    [1] * T   # Availability for Resource 2
+    [1] * T for _ in range(M)  # Availability for Resource 1 and Resource 2
 ]
 processing_times_resource = [
     [1, 0.8, 1.5, 0.7, 1, 0.8, 1.5, 0.7, 1, 0.8],  # Processing times for Resource 1
     [0.5, 1, 2, 0.7, 0.5, 1, 2, 0.7, 0.5, 1]       # Processing times for Resource 2
 ]
-
-# Print the dimensions of data structures for debugging
-print("Number of Orders (N):", N)
-print("Number of Resources (M):", M)
-print("Number of Time Periods (T):", T)
-print("Resource Availabilities Dimensions:", len(resource_availabilities), len(resource_availabilities[0]))
 
 # Create Pyomo model
 model = ConcreteModel()
@@ -39,7 +33,7 @@ model.x = Var(range(N), range(M), range(T), within=Binary)
 
 # Objective function: Minimize total production cost
 model.objective = Objective(
-    expr=sum(processing_times_resource[i][j] * model.x[i, j, t] for i in range(N) for j in range(M) for t in range(T)),
+    expr=sum(processing_times[i] * model.x[i, j, t] for i in range(N) for j in range(M) for t in range(T)),
     sense=minimize
 )
 
@@ -68,6 +62,6 @@ for i in range(N):
     for j in range(M):
         for t in range(T):
             if model.x[i, j, t].value == 1:
-                print(f"Order {i+1} is processed on Resource {j+1} at Time {t+1}")
+                print(f"Order {i + 1} is processed on Resource {j + 1} at Time {t + 1}")
 
 print("Total Production Cost:", model.objective())
